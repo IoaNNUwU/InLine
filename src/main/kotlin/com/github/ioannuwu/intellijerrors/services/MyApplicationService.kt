aa1c9000c23611d114416f557d386d381802697b
GitHub Action <action@github.com>
2023-01-27 15:49:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/ioannuwu/intellijerrors/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ioannuwu/intellijerrors/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ioannuwu.intellijerrors.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ioannuwu.intellijerrors.MyBundle
 
 class MyApplicationService {
 
