76b99895e80d15466b7dd0bb2d50f9167f3b70df
IoaNNUwU <ioannxd@gmail.com>
2023-03-11 12:55:28 +0200

    fix tests

--- src/test/java/com/ioannuwu/inline/data/DefaultSettingsTest.java
+++ src/test/java/com/ioannuwu/inline/data/DefaultSettingsTest.java
@@ -8,10 +8,10 @@ public class DefaultSettingsTest {
 
     @Test
     public void defaultSettingsTest() {
-        assertNotNull("", DefaultSettings.ERROR);
-        assertNotNull("", DefaultSettings.WARNING);
-        assertNotNull("", DefaultSettings.WEAK_WARNING);
-        assertNotNull("", DefaultSettings.IGNORE_LIST);
-        assertNotNull("", DefaultSettings.FONT);
+        assertNotNull("There is no ERROR in default settings", DefaultSettings.ERROR);
+        assertNotNull("There is no WARNING in default settings", DefaultSettings.WARNING);
+        assertNotNull("There is no WEAK WARNING in default settings", DefaultSettings.WEAK_WARNING);
+        assertNotNull("There is no IGNORE LIST in default settings", DefaultSettings.IGNORE_LIST);
+        assertNotNull("There is no FONT in default settings", DefaultSettings.FONT);
     }
 }
