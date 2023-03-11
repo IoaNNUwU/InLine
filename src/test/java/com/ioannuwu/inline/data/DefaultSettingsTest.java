739403b67aa0b75416cb723010a7e6598dc4603c
IoaNNUwU <ioannxd@gmail.com>
2023-03-11 13:03:28 +0200

    add additional tests

--- src/test/java/com/ioannuwu/inline/data/DefaultSettingsTest.java
+++ src/test/java/com/ioannuwu/inline/data/DefaultSettingsTest.java
@@ -13,5 +13,8 @@ public class DefaultSettingsTest {
         assertNotNull("There is no WEAK WARNING in default settings", DefaultSettings.WEAK_WARNING);
         assertNotNull("There is no IGNORE LIST in default settings", DefaultSettings.IGNORE_LIST);
         assertNotNull("There is no FONT in default settings", DefaultSettings.FONT);
+        assertNotNull("There is no ERROR ICON in default settings", DefaultSettings.Icons.ERROR);
+        assertNotNull("There is no SERVER ERROR ICON in default settings", DefaultSettings.Icons.SERVER_ERROR);
+        assertNotNull("There is no WEAK WARNING ICON in default settings", DefaultSettings.Icons.WEAK_WARNING);
     }
 }
