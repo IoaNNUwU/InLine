5cd79cd74a9faad19192646640283253cad99285
IoaNNUwU <ioannxd@gmail.com>
2023-08-22 15:50:07 +0200

    Fix IndexOutOfBoundsException

--- src/main/kotlin/com/ioannuwu/inline2/pluginlogic/editormodel/EditorModelImpl.kt
+++ src/main/kotlin/com/ioannuwu/inline2/pluginlogic/editormodel/EditorModelImpl.kt
@@ -16,8 +16,11 @@ class EditorModelImpl(private val editor: Editor) : EditorModel {
         offset: Int,
         renderer: EditorCustomElementRenderer
     ): Disposable {
-
-        return editor.inlayModel.addAfterLineEndElement(offset, true, renderer)!!
+        return try {
+            editor.inlayModel.addAfterLineEndElement(offset, true, renderer)!!
+        } catch (e: IndexOutOfBoundsException) {
+            Disposable { }
+        }
     }
 
     override fun addUnderLineElement(
@@ -25,41 +28,49 @@ class EditorModelImpl(private val editor: Editor) : EditorModel {
         renderer: EditorCustomElementRenderer
     ): Disposable {
 
-        return editor.inlayModel.addBlockElement(offset, true, false, 9, renderer)!!
+        return try {
+            editor.inlayModel.addBlockElement(offset, true, false, 9, renderer)!!
+        } catch (e: IndexOutOfBoundsException) {
+            Disposable { }
+        }
+
     }
 
     override fun addLineHighlighter(offset: Int, backgroundColor: Color): Disposable {
 
-        val lineNumber = try {
-            editor.document.getLineNumber(offset)
+        return try {
+            val lineNumber = editor.document.getLineNumber(offset)
+
+            val highlighter =
+                editor.markupModel.addLineHighlighter(lineNumber, 9, BackgroundAttributes(backgroundColor))
+
+            highlighter.makeDisposable { it.dispose() }
+
         } catch (e: IndexOutOfBoundsException) {
-            editor.document.lineCount
+            Disposable { }
         }
-
-        val highlighter = editor.markupModel.addLineHighlighter(lineNumber, 9, BackgroundAttributes(backgroundColor))
-
-        return highlighter.makeDisposable { it.dispose() }
     }
 
     override fun addGutterIcon(offset: Int, icon: Icon): Disposable {
 
-        val lineNumber = try {
-            editor.document.getLineNumber(offset)
+        return try {
+            val lineNumber = editor.document.getLineNumber(offset)
+
+            val highlighter = editor.markupModel.addLineHighlighter(
+                lineNumber, 10,
+                TextAttributes(null, null, null, null, 0)
+            )
+
+            highlighter.gutterIconRenderer = object : GutterIconRenderer() {
+                override fun equals(other: Any?): Boolean = (other as? GutterIconRenderer)?.icon == icon
+                override fun hashCode(): Int = icon.hashCode()
+                override fun getIcon(): Icon = icon
+            }
+
+            highlighter.makeDisposable { it.dispose() }
+
         } catch (e: IndexOutOfBoundsException) {
-            editor.document.lineCount
+            Disposable { }
         }
-
-        val highlighter = editor.markupModel.addLineHighlighter(
-            lineNumber, 10,
-            TextAttributes(null, null, null, null, 0)
-        )
-
-        highlighter.gutterIconRenderer = object : GutterIconRenderer() {
-            override fun equals(other: Any?): Boolean = (other as? GutterIconRenderer)?.icon == icon
-            override fun hashCode(): Int = icon.hashCode()
-            override fun getIcon(): Icon = icon
-        }
-
-        return highlighter.makeDisposable { it.dispose() }
     }
 }
